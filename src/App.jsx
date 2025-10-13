b11f9a6dd72192e2eda66e460761a5ad2a95bdad
Nizamudeen N <nizamudeen@solwyz.com>
2025-10-13 11:10:23 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 import Login from './auth/Login';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Login />} /> {/* Default route under Layout */}
-          <Route path="login" element={<Login />} /> {/* /login route */}
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Login />} />  {/* Default route under Layout */}
+        <Route path="login" element={<Login />} />  {/* /login route */}
+      </Route>
+    </Routes>
   );
 }
 
