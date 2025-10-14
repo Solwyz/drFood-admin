6dc168002b70f3b70eef3c9cf4de99e2df91524b
Nizamudeen N <nizamudeen@solwyz.com>
2025-10-14 10:00:02 +0530

    feat:implemented enquriy page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Layout from './Layout';
-import Login from './auth/Login';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Login from "./auth/Login";
+import EnquiryPage from "./pages/enquiryPage/EnquiryPage";
 
 function App() {
   return (
     <Routes>
       {/* <Route index element={<Login />} />  Default route under Layout */}
-      <Route path="login" element={<Login />} />  {/* /login route */}
+      <Route path="login" element={<Login />} /> {/* /login route */}
       <Route path="/" element={<Layout />}>
-
+        <Route path="enquiry" element={<EnquiryPage />} />
       </Route>
     </Routes>
   );
