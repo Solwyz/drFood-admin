4dda70aef0dab9077daf7dba2c387260badaafb3
Ruthe46 <ruthinlosh.web.solwyz@gmail.com>
2025-10-14 14:45:13 +0530

    feat:product list page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Login from "./auth/Login";
 import EnquiryPage from "./pages/enquiryPage/EnquiryPage";
+import ProductPage from './Pages/ProductPage/ProductPage';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Route path="login" element={<Login />} /> {/* /login route */}
       <Route path="/" element={<Layout />}>
         <Route path="enquiry" element={<EnquiryPage />} />
+        <Route path='products' element={<ProductPage />} />
       </Route>
     </Routes>
   );
