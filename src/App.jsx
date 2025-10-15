3da2ab7159579accaa0184b26c8641bcc179b1ec
Nizamudeen N <nizamudeen@solwyz.com>
2025-10-15 17:47:31 +0530

    feat:blogPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Layout from "./Layout";
 import Login from "./auth/Login";
 import EnquiryPage from "./pages/enquiryPage/EnquiryPage";
 import ProductPage from './Pages/ProductPage/ProductPage';
+import BlogPage from "./pages/blogPage/BlogPage";
+import BlogForm from "./pages/blogPage/BlogForm";
 
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route path="enquiry" element={<EnquiryPage />} />
         <Route path='products' element={<ProductPage />} />
+        <Route path='blogs' element={<BlogPage />} />
+        <Route path='blogPageForm' element={<BlogForm/>} />
+          <Route path='blogPageForm/:blogId' element={<BlogForm />} />
       </Route>
     </Routes>
   );
